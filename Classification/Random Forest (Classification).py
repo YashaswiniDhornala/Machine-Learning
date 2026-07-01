1050fbd78fc4f302d4a6a889fe3c53ccf0b45f0b
Yashaswini Dhornala <yash17dhornala@gmail.com>
2026-07-01 21:03:53 +0530

    Add files via upload

---
+++ Classification/Random Forest (Classification).py
@@ -0,0 +1,67 @@
+# Importing Libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Import the Dataset
+dataset = pd.read_csv(r"C:\Users\hp\Downloads\logit classification.csv")
+X= dataset.iloc[:,[2,3]].values
+y= dataset.iloc[:,-1].values
+
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20,random_state = 0)
+
+# Feature Scaling
+#from sklearn.preprocessing import StandardScaler
+#sc = StandardScaler()
+#X_train = sc.fit_transform(X_train)
+#X_test = sc.transform(X_test)
+
+# Training the Random Forest model on the Training set
+# test case 1
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier()
+classifier.fit(X_train, y_train)
+
+#Test case 2
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(random_state=0)
+classifier.fit(X_train, y_train)
+
+#Test case 3
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(random_state=0, criterion='')
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+# This is to get the Models Accuracy 
+from sklearn.metrics import accuracy_score 
+ac = accuracy_score(y_test, y_pred)
+print(ac) 
+
+
+# This is to get the Classification Report
+from sklearn.metrics import classification_report
+cr = classification_report(y_test, y_pred)
+cr
+
+bias = classifier.score(X_train,y_train)
+bias
+
+variance = classifier.score(X_test,y_test)
+variance
+
+
+
+
+
+
